54ac99d1223701aa9a04290d5fbaaf2a479fde20
User1 <iegor.lourens@gmail.com>
2026-05-26 22:36:51 +0300

    adding first files

---
+++ pipeline/pipeline.py
@@ -0,0 +1,18 @@
+import sys
+import pandas as pd
+
+print('Аргументы' , sys.argv)
+
+month = int(sys.argv[1])
+
+df = pd.DataFrame({'Day': [1, 2],
+                   'num_passengers': [3, 4]})
+df['month'] = month
+
+df.to_parquet(f'output_{month}.parquet')
+
+print(df.head())
+
+
+
+print('Месяц', month)
